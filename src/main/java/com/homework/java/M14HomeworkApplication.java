e035b383c8584bd332266b3cd259fb5da42038f4
ZabeidaNazar <nazarzabeida@gmail.com>
2024-01-07 14:59:54 +0200

    Initial commit

---
+++ src/main/java/com/homework/java/M14HomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.homework.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M14HomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M14HomeworkApplication.class, args);
+	}
+
+}
